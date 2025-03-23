463e83ea96623e8f28c8dc1bdfe298f87649520a
jker.m.o <jkermateoorcino@gmail.com>
2025-03-23 21:49:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Pokedex
+A JavaScript-based Pokedex website with search, filtering, and responsive design. This project is built using HTML, CSS, and JavaScript to provide a user-friendly experience for browsing Pokemon data.
+# Features
+- Search capabilities
+- Filtering
+- Responsive designs
+# Getting Started
+1. Clone the repository.
+2. Install Live Server (by ritwickdey) from the Extensions tab in VS Code.
+3. Open index.html from the client folder, then click "Go Live" in the status bar to start or stop the server.
+4. The page will load automatically. If not, manually access the application at http://127.0.0.1:5500/client/index.html.
+# Technologies Used
+- JavaScript for frontend and backend scripting.
+- HTML for frontend structure.
+- CSS for styling.
+# Project Structure
+```bash
+/pokedex-project
+├── /client                  # Frontend application
+│   ├── /assets              # Contains images and SVGs
+│   ├── pokedex.js           # Fetches and displays Pokemon data
+│   ├── pokemon-detail.js    # Manages Pokemon detail page interactions
+│   ├── search.js            # Implements search and filtering functionality
+│   ├── style.css            # Defines styles and responsive design
+│   ├── detail.html          # Pokemon details page
+│   ├── index.html           # Main homepage displaying Pokemon list
+└── /server                  # Reserved for backend logic if added later
+```
---
+++ client/pokedex.js
@@ -0,0 +1,107 @@
+const MAX_POKEMON = 649;
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
+
+// Here, we are creating an empty array.
+let allPokemons = [];
+
+// Then, we are fetching data and putting it to the array.
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
+.then((response) => response.json())
+.then((data) => {
+    allPokemons = data.results;
+    displayPokemons(allPokemons); // We call the displayPokemons function for displaying.
+});
+
+// This function will get the pokemon data before redirecting to the details.html
+async function fetchPokemonDataBeforeRedirect(id) {
+    try {
+        const [pokemon, pokemonSpecies] = await Promise
+            .all([fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            .then((res) =>
+                res.json()
+            ),
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`)
+            .then((res) =>
+                res.json()
+            ),
+        ]);
+        return true
+    } catch (error) {
+        console.error("Failed to fetch Pokemon data before redirect")
+    }
+}
+
+// Display all the fetched data from the api.
+function displayPokemons(pokemon) {
+    listWrapper.innerHTML = "";
+
+    pokemon.forEach((pokemon) => {
+        const pokemonID = pokemon.url.split("/")[6];
+        const listItem = document.createElement("div");
+        listItem.className = "list-item";
+        listItem.innerHTML = `
+            <div class="number-wrap">
+                <p class="caption-fonts">#${pokemonID}</p>
+            </div>
+            <div class="img-wrap">
+                <img src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/${pokemonID}.svg" alt="${pokemon.name}" />
+            </div>
+            <div class="name-wrap">
+                <p class="body3-fonts">#${pokemon.name}</p>
+            </div>
+        `;
+
+        listItem.addEventListener("click", async () => {
+            const success = await
+            fetchPokemonDataBeforeRedirect(pokemonID);
+            if (success) {
+                window.location.href = `./detail.html?id=${pokemonID}`;
+            }
+        });
+
+        listWrapper.appendChild(listItem);
+    });
+}
+
+searchInput.addEventListener("keyup", handleSearch);
+
+// This function will handle search/filter base on pokemon name or id.
+function handleSearch() {
+    const searchTerm = searchInput.value.toLowerCase();
+    let filteredPokemons;
+
+    if (numberFilter.checked) {
+        filteredPokemons = allPokemons.filter((pokemon) => {
+            const pokemonID = pokemon.url.split("/")[6];
+            return pokemonID.startsWith(searchTerm);
+        });
+    } else if (nameFilter.checked) {
+        filteredPokemons = allPokemons.filter((pokemon) => {
+            return pokemon.name.toLowerCase().startsWith(searchTerm);
+        });
+    } else {
+        filteredPokemons = allPokemons;
+    }
+
+    displayPokemons(filteredPokemons);
+
+    if (filteredPokemons.length === 0) {
+        notFoundMessage.style.display = "block";
+    } else {
+        notFoundMessage.style.display = "none";
+    }
+}
+
+const closeButton = document.querySelector(".search-close-icon");
+closeButton.addEventListener("click", clearSearch);
+
+// This function will clear searches from the search box.
+function clearSearch() {
+    searchInput.value = "";
+    displayPokemons(allPokemons);
+    notFoundMessage.style.display = "none";
+}
